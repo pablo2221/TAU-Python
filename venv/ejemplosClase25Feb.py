8db22655a1e91f0a2e36890925b5ce49339e2da0
pablomac <pablo.2221@hotmail.com>
2021-02-25 19:57:20 -0600

    Practica Jueves

--- venv/ejemplosClase25Feb.py
+++ venv/ejemplosClase25Feb.py
@@ -5,3 +5,31 @@ def bienvenida ():
     return
 
 bienvenida()
+
+#Ejemplo: 2 funcion que imprime con parametros
+def bienvenida (nombre):
+    print("Bienvenido a python "+nombre)
+    return
+
+bienvenida("Pablo")
+
+# Ejemplo: 3 calculo de area de triangulo
+
+def areaTriangulo(base, altura):
+    area = (base*altura)/2
+    print("El area del triangulo es: "+str(area))
+    return
+
+areaTriangulo(2, 4)
+areaTriangulo(4, 7)
+
+# Ejemplo: 4 calcular hipotenusa de un triangulo
+
+import math
+
+def hipotenusa(c1,c2):
+      h=math.sqrt((pow(c1,2)+pow(c2,2)))
+      return h
+
+print("La hipotenusa del triangulo es :", hipotenusa(3,4))
+
