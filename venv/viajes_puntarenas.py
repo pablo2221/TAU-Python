5eae139f2cf09d05c286505c6e331074eac2165d
pablomac <pablo.2221@hotmail.com>
2021-03-01 15:32:40 -0600

    Proyecto calculadora areas

--- venv/viajes_puntarenas.py
+++ venv/viajes_puntarenas.py
@@ -1,1 +1,3 @@
-cantidad
+cantidad_personas = int(input("Por favor ingrese la cantidad de turistas que necesitan transporte: "))
+print("Se requieren "+str(cantidad_personas/25)+" buses para transportar a las personas")
+
